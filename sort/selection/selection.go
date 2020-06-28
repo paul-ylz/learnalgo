637959aa779ced7a82a2c3bc9d6b82488ef356d7
Paul Yeoh <paulyeoh.lz@gmail.com>
2020-06-28 21:49:36 +0800

    Added reverse selection sort

--- sort/selection/selection.go
+++ sort/selection/selection.go
@@ -7,11 +7,27 @@ func selectionSort(args []int) []int {
 	for i := 0; i < len(list)-1; i++ {
 		smallestIndex := i
 		for j := i; j <= len(list)-1; j++ {
-			if list[j] <= list[smallestIndex] {
+			if list[j] < list[smallestIndex] {
 				smallestIndex = j
 			}
 		}
 		list[i], list[smallestIndex] = list[smallestIndex], list[i]
+	}
+	return list
+}
+
+func selectionSortReverse(args []int) []int {
+	list := make([]int, len(args))
+	copy(list, args)
+
+	for i := 0; i < len(list)-1; i++ {
+		largestIndex := i
+		for j := i; j <= len(list)-1; j++ {
+			if list[j] > list[largestIndex] {
+				largestIndex = j
+			}
+		}
+		list[i], list[largestIndex] = list[largestIndex], list[i]
 	}
 	return list
 }
--- sort/selection/selection_test.go
+++ sort/selection/selection_test.go
@@ -13,3 +13,12 @@ func TestSelectionSort(t *testing.T) {
 		t.Errorf("selectionSort(%v) = %v got; want %v", args, got, want)
 	}
 }
+
+func TestSelectionSortReverse(t *testing.T) {
+	args := []int{29, 10, 14, 37, 14}
+	want := []int{37, 29, 14, 14, 10}
+	got := selectionSortReverse(args)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("selectionSortReverse(%v) = %v got; want %v", args, got, want)
+	}
+}
